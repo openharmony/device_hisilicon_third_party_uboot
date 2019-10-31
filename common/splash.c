openharmony/third_party_u-boot

commit f281acb7ebf8c713274e97e55553b5cd521f9065

splash: fix splash banner output
Old splash code in cfb_console driver displayed U-Boot version
string by default. Restore this behaviour for DM_VIDEO enabled
configurations.

Signed-off-by: Anatolij Gustschin <agust@denx.de>
Reported-by: Fabio Estevam <festevam@gmail.com>

common/splash.c
@@ -112,6 +112,42 @@ void splash_get_pos(int *x, int *y)
 }
 #endif /* CONFIG_SPLASH_SCREEN_ALIGN */
 
+#if defined(CONFIG_DM_VIDEO) && !defined(CONFIG_HIDE_LOGO_VERSION)
+
+#ifdef CONFIG_VIDEO_LOGO
+#include <bmp_logo.h>
+#endif
+#include <dm.h>
+#include <video_console.h>
+#include <video_font.h>
+
+void splash_display_banner(void)
+{
+	struct udevice *dev;
+	char buf[DISPLAY_OPTIONS_BANNER_LENGTH];
+	int col, row, ret;
+
+	ret = uclass_get_device(UCLASS_VIDEO_CONSOLE, 0, &dev);
+	if (ret)
+		return;
+
+#ifdef CONFIG_VIDEO_LOGO
+	col = BMP_LOGO_WIDTH / VIDEO_FONT_WIDTH + 1;
+	row = BMP_LOGO_HEIGHT / VIDEO_FONT_HEIGHT + 1;
+#else
+	col = 0;
+	row = 0;
+#endif
+
+	display_options_get_banner(false, buf, sizeof(buf));
+	vidconsole_position_cursor(dev, col, 1);
+	vidconsole_put_string(dev, buf);
+	vidconsole_position_cursor(dev, 0, row);
+}
+#else
+static inline void splash_display_banner(void) { }
+#endif /* CONFIG_DM_VIDEO && !CONFIG_HIDE_LOGO_VERSION */
+
 /*
  * Common function to show a splash image if env("splashimage") is set.
  * Is used for both dm_video and lcd video stacks. For additional
@@ -135,6 +171,14 @@ int splash_display(void)
 
 	splash_get_pos(&x, &y);
 
-	return bmp_display(addr, x, y);
+	ret = bmp_display(addr, x, y);
+
+	/* Skip banner output on video console if the logo is not at 0,0 */
+	if (x || y)
+		goto end;
+
+	splash_display_banner();
+end:
+	return ret;
 }
 #endif

openharmony/device_hisilicon_third_party_uboot

commit f399e6cdf31e88e1d38f54d6d2d2a771fdf307f3

splash: fix build breakage with newer llvm-7
Some configurations (i.e. sandbox) result in unused
splash_display_banner() and we get build errors like:

  common/splash.c:148:20: error: unused function 'splash_display_banner' [-Werror,-Wunused-function]
  static inline void splash_display_banner(void) { }

Drop empty inline function and add guards around the call.

Fixes: f281acb7ebf8 ("splash: fix splash banner output")
Signed-off-by: Anatolij Gustschin <agust@denx.de>
Reported-by: Tom Rini <trini@konsulko.com>
Reviewed-by: Tom Rini <trini@konsulko.com>

common/splash.c
@@ -144,8 +144,6 @@ void splash_display_banner(void)
 	vidconsole_put_string(dev, buf);
 	vidconsole_position_cursor(dev, 0, row);
 }
-#else
-static inline void splash_display_banner(void) { }
 #endif /* CONFIG_DM_VIDEO && !CONFIG_HIDE_LOGO_VERSION */
 
 /*
@@ -177,7 +175,9 @@ int splash_display(void)
 	if (x || y)
 		goto end;
 
+#if defined(CONFIG_DM_VIDEO) && !defined(CONFIG_HIDE_LOGO_VERSION)
 	splash_display_banner();
+#endif
 end:
 	return ret;
 }