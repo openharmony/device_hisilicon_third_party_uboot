6ac8f4207c142cc08dfa21c7b88ae19d83cc6bfe
maltose214 <mashuai3@huawei.com>
2021-09-10 20:40:35 +0800

    add update status to metadata
    Signed-off-by: maltose214 <mashuai3@huawei.com>

--- u-boot-2020.01/product/hiupdate/auto_update_adaptation.c
+++ u-boot-2020.01/product/hiupdate/auto_update_adaptation.c
@@ -1059,7 +1059,7 @@ static int update_to_flash(void)
 	int cnt;
 	int uboot_updated = 0;
 	char buf[NAME_MAX_LEN] = {0};
-    int update_status = 1;
+	int update_status = 1;
 
 #ifdef CONFIG_AUTO_OTA_UPDATE
 	if (g_is_ota) {
@@ -1154,7 +1154,7 @@ static int update_to_flash(void)
 		} while (res < 0);
 	}
 	
-    set_update_status(update_status);
+	set_update_status(update_status);
 
 	if (uboot_updated == 1) {
         printf("update success!\n");
